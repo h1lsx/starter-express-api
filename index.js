96d53c7330bd252705b402f071c3cacdd12c22a8
h1lsx <139397050+h1lsx@users.noreply.github.com>
2023-10-25 14:57:57 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 app.use(express.static('static'))
 app.all('/', (req, res) => {
-    console.log("Just got a request!")
+    res.send("Just got a request!")
     res.send(tr10)
 })
 app.listen(process.env.PORT || 3000)
