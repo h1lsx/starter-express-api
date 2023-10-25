dc69beedd36ce8ef04d0021f127b905a77f86e21
h1lsx <139397050+h1lsx@users.noreply.github.com>
2023-10-25 15:00:46 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-app.use(express.static('static'))
+app.use(express.static('/static'))
 app.all('/', (req, res) => {
     res.send("Just got a request!")
     res.send(tr10)
